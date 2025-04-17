8919cb2f0adf1d4f3ad162cba92190cc1f16e6f1
Pravi Jain <parasjain.pj902@gmail.com>
2025-04-17 14:33:31 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
-import { Skills } from "./components/Skills/Skills";
+import {Skills} from "./components/Skills/Skills"
 import { Profile } from "./components/Profile/profile";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
